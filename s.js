cc3c903bca2aaf81fbec6ba57c81a9e227071c4d
Asadkanasiro <60060209+asadkanasiro@users.noreply.github.com>
2023-10-12 15:00:00 +0500

    Add files via upload

---
+++ s.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+            // Your JavaScript code goes here
+            function greet(name) {
+                return "Hello, " + name + "!";
+            }
+
+            let userName = "John";
+            let greeting = greet(userName);
+
+            document.getElementById("output").innerHTML = greeting;
+        });
